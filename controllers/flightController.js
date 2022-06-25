7d7f293bcc8cb0d1f40a913bbe4e066bf90c164c
McConnell <ik.ugwuanyi@gmail.com>
2022-06-25 16:53:41 +0100

    initial commit

--- controllers/flightController.js
+++ controllers/flightController.js
@@ -1,6 +1,55 @@
-exports.example = (req, res) => {
-    console.log("example")
-    res.send("Flight example")
-}
+const fs = require("fs");
+const path = require("path");
+const pathToDb = path.join(__dirname, "../models", "/flights.json");
 
+const flights = [];
 
+exports.bookFlight = (req, res) => {
+  const { title, time, price, date } = req.body;
+
+  if (!title || !time || !price || !date) {
+    return res.status(400).json({ msg: "One or more fields missing" });
+  }
+
+  const data = { id: Date.now(), ...req.body };
+
+  flights.push(data);
+
+  //const jsonData = JSON.stringify(flights);
+  fs.writeFileSync(pathToDb, flights, { flag: "a" });
+
+  res.status(201).json(data);
+};
+
+exports.getFlights = (req, res) => {
+  const flights = flightDb;
+
+  res.status(200).json(flights);
+};
+
+exports.getFlight = (req, res) => {
+  const { id } = req.params;
+
+  const flight = flightDb.find({ id: id });
+
+  res.status(200).json(flight);
+};
+
+exports.updateFlight = (req, res) => {
+  const { id } = req.params;
+  const newData = req.body;
+
+  const flight = flightDb.find({ id: id });
+
+  // Update flight
+
+  const updatedFlight = flightDb.find({ id: id });
+
+  res.status(200).json(updatedFlight);
+};
+
+exports.deleteFlight = (req, res) => {
+  const { id } = req.params;
+
+  res.status(204).json({ msg: "Flight Deleted" });
+};
--- routes/flightRoute.js
+++ routes/flightRoute.js
@@ -1,9 +1,12 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
-const controller = require('../controllers/flightController');
+const controller = require("../controllers/flightController");
 
-router.get('/', controller.example)
+router.post("/", controller.bookFlight);
+router.get("/", controller.getFlights);
+router.get("/:id", controller.getFlight);
+router.patch("/:id", controller.updateFlight);
+router.delete("/:id", controller.deleteFlight);
 
 module.exports = router;
-
